3e0379704ed9e36d855d670491da1f7ce80d56e3
Nayomi-Samarapperuma <nayomi985571490@gmail.com>
2024-01-02 20:00:26 +0530

    first commit

---
+++ resources/views/components/side.blade.php
@@ -0,0 +1,29 @@
+<div class="sidenav-footer mx-3 my-3">
+    <div class="card card-plain shadow-none" id="sidenavCard">
+    <img class="w-60 mx-auto" src="../../assets/img/illustrations/icon-documentation.svg" alt="sidebar_illustration">
+    <div class="card-body text-center p-3 w-100 pt-0">
+    <div class="docs-info">
+    <h6 class="mb-0">Need help?</h6>
+    <p class="text-xs font-weight-bold mb-0">Please check our docs</p>
+    </div>
+    </div>
+    </div>
+    <a href="https://www.creative-tim.com/learning-lab/bootstrap/overview/argon-dashboard" target="_blank" class="btn btn-dark btn-sm w-100 mb-3">Documentation</a>
+    </div>
+    </aside>
+    <div class="sidenav-toggler sidenav-toggler-inner d-xl-block d-none ">
+        <a href="javascript:;" class="nav-link p-0">
+        <div class="sidenav-toggler-inner">
+        <i class="sidenav-toggler-line bg-white"></i>
+        <i class="sidenav-toggler-line bg-white"></i>
+        <i class="sidenav-toggler-line bg-white"></i>
+        </div>
+        </a>
+        </div>
+        <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+            <div class="sidenav-toggler-inner">
+            <i class="sidenav-toggler-line bg-white"></i>
+            <i class="sidenav-toggler-line bg-white"></i>
+            <i class="sidenav-toggler-line bg-white"></i>
+            </div>
+            </a>
---
+++ resources/views/libraries/styles.blade.php
@@ -0,0 +1,12 @@
+
+<link href="{{ asset('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet') }}" />
+<link href="{{ asset('../../assets/css/nucleo-icons.css" rel="stylesheet') }}" />
+<link href="{{ asset('../../assets/css/nucleo-svg.css" rel="stylesheet') }}" />
+<link href="{{ asset('../../assets/css/nucleo-svg.css" rel="stylesheet') }}" />
+<link id="pagestyle" href="{{ asset('../../assets/css/argon-dashboard.min.css?v=2.0.5" rel="stylesheet') }}" />
+<style>
+    .async-hide {
+      opacity: 0 !important
+    }
+  </style>
+
